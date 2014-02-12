a5d42ea53eadfd95dfa078e75d9b1d2e4ae53718
Chris Larson <chris@server.(none)>
2014-02-12 08:26:34 -0600

    Framework-angular and bootstrap

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('m2xApp', ['ngCookies', 'ngSanitize', 'ngRoute', 'ngAnimate'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('m2xApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
